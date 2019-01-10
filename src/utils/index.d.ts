1560aef7e4ef20fe6aa3a1c8a627d0e2ab128d58
Wolfger Schramm <wolfger@spearwolf.de>
2019-01-10 11:24:39 +0100

    fix typings add npm task

--- src/utils/index.d.ts
+++ src/utils/index.d.ts
@@ -2,9 +2,9 @@
 export function findNextPowerOf2(x: number): number;
 export function get(obj: Object, path: string): unknown;
 export function hexCol2rgb(hexColor: string): [number, number, number];
-export function hexCol2rgba(hexColor: string, alpha: number = 255): [number, number, number, number];
+export function hexCol2rgba(hexColor: string, alpha?: number): [number, number, number, number];
 export function isPowerOf2(n: number): boolean;
-export function makeCircleCoords(steps: number, radius: number = 1): Array<number>;
+export function makeCircleCoords(steps: number, radius?: number): Array<number>;
 export function maxOf(a: number, b: number): number;
 export function pick(names: Array<string>): (obj: Object) => Object;
 export function sample<T>(arr: Array<T>): T;
