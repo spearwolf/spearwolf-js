786a5c54f850903f8692e85421d9f5c428b186c6
Wolfger Schramm <wolfger@spearwolf.de>
2019-02-27 16:40:10 +0100

    improve updateBuffers()

--- src/three/SpriteGroupBufferGeometry.js
+++ src/three/SpriteGroupBufferGeometry.js
@@ -9,6 +9,7 @@ export class SpriteGroupBufferGeometry extends THREE.BufferGeometry {
     super();
 
     this.type = 'spearwolf.SpriteGroupBufferGeometry';
+
     this.parameters = {
       spriteGroup,
     };
@@ -18,17 +19,45 @@ export class SpriteGroupBufferGeometry extends THREE.BufferGeometry {
     const { descriptor } = spriteGroup;
     const { voArray } = spriteGroup.voPool;
 
+    /**
+     * @private
+     * @type {THREE.InterleavedBuffer[]}
+     */
+    this._buffers = [];
+
+    /**
+     * @private
+     * @type {Map<THREE.InterleavedBuffer>}
+     */
+    this._bufMap = new Map();
+
     Object.keys(descriptor.attr).forEach(attrName => {
 
       const attr = descriptor.attr[attrName];
-      const typedArray = voArray[`${attr.type}Array`];
 
-      const stride = descriptor.bytesPerVertex / attr.bytesPerElement;
-      const buffer = new THREE.InterleavedBuffer(typedArray, stride);
+      let buffer = this._bufMap.get(attr.type);
+
+      if (!buffer) {
+        const typedArray = voArray[`${attr.type}Array`];
+        const stride = descriptor.bytesPerVertex / typedArray.BYTES_PER_ELEMENT;
+
+        buffer = new THREE.InterleavedBuffer(typedArray, stride);
+        buffer.setDynamic(true); // TODO
+
+        this._buffers.push(buffer);
+        this._bufMap.set(attr.type, buffer);
+      }
+
       const bufferAttr = new THREE.InterleavedBufferAttribute(buffer, attr.size, attr.offset);
 
       this.addAttribute(attrName, bufferAttr);
 
+    });
+  }
+
+  updateBuffers() {
+    this._buffers.forEach((buf) => {
+      buf.needsUpdate = true;
     });
   }
 
