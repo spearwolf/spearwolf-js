394a0ad8740c4a5f2ad0802eea3958aada15345b
Wolfger Schramm <wolfger@spearwolf.de>
2019-03-14 11:21:26 +0100

    improve instanced geometry example

--- examples/src/instancedGeometry.js
+++ examples/src/instancedGeometry.js
@@ -11,11 +11,11 @@ import {
   SpriteGroupInstancedBufferGeometry,
   SpriteGroupMesh,
   VODescriptor,
-  hexCol2rgba,
+  hexCol2rgb,
   sample,
 } from '../../src';
 
-const init = async ({ scene }) => {
+const init = async ({ canvas, scene }) => {
 
   // ----------------------------------------------------------------------------------
   //
@@ -23,14 +23,14 @@ const init = async ({ scene }) => {
   //
   // ----------------------------------------------------------------------------------
 
-  const ambientLight = new THREE.AmbientLight(0x79b3ce);
+  const ambientLight = new THREE.AmbientLight(0xf0f0c0);
   scene.add(ambientLight);
 
-  const hemisphereLight = new THREE.HemisphereLight(0x79b3ce, 0xd6d6ba, 1);
+  const hemisphereLight = new THREE.HemisphereLight(0xf0f0f0, 0xffffff, 1);
   scene.add(hemisphereLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  directionalLight.position.set(-8, 5, 1);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(0, 0, 10);
   scene.add(directionalLight);
 
   // ----------------------------------------------------------------------------------
@@ -43,9 +43,18 @@ const init = async ({ scene }) => {
 
     attributes: {
 
-      move: ['tx', 'ty', 'tz'],
+      move: ['x', 'y', 'z'],
 
-      tint: { type: 'uint8', scalars: ['r', 'g', 'b', 'a'] },
+      tint: { type: 'uint8', scalars: ['r', 'g', 'b'] },
+
+    },
+
+    methods: {
+
+      setColor(color) {
+        const colors = hexCol2rgb(color);
+        this.setTint(...colors);
+      }
 
     },
 
@@ -68,70 +77,85 @@ const init = async ({ scene }) => {
 
   // ----------------------------------------------------------------------------------
   //
+  // create some sprites
+  //
+  // ----------------------------------------------------------------------------------
+
+  const COLORS = [
+    'b7fbff',
+    'fff6be',
+    'ffe0a3',
+    'ffa1ac',
+  ];
+
+  makeCircleCoords(100, 650, (x, y, z) => {
+    const sprite = spriteGroup.createSprite();
+    sprite.setMove(x, y, z);
+    sprite.setColor(sample(COLORS));
+  });
+
+  // ----------------------------------------------------------------------------------
+  //
+  // create some custom uniforms
+  //
+  // ----------------------------------------------------------------------------------
+
+  const timeUniform = { value: 0.0 };
+
+  canvas.addEventListener('frame', ({ now }) => {
+
+    timeUniform.value = now % Math.PI * 2;
+
+  });
+
+  // ----------------------------------------------------------------------------------
+  //
   // create base geometry
   //
   // ----------------------------------------------------------------------------------
 
   const baseGeometry = new THREE.SphereBufferGeometry(13, 10, 10);
 
-  const material = new THREE.MeshLambertMaterial({
-    color: 0x999999,
-  });
+  const material = new THREE.MeshLambertMaterial();
 
   // @ts-ignore
   material.onBeforeCompile = (shader) => {
 
-    // #include <color_vertex>
-    //    vColor.xyz = color.xyz;
-
-    // shader.vertexShader = `
-    //   attribute vec3 tint;
-    //   ${shader.vertexShader}
-    // `;
-
-    shader.vertexShader = shader.vertexShader.replace(
-      '#include <color_vertex>',
-      `
-        #ifdef USE_COLOR
-          vColor.xyz = vec3( 0.3, 0.8, 0.0 );
-        #endif
-      `,
-    );
-    // vColor.xyz = vec3( 255.0 / tint.x, 255.0 / tint.y, 255.0 / tint.z );
-
-    // #include <begin_vertex>
-    //    vec3 transformed = vec3( position );
+    shader.uniforms.time = timeUniform;
 
     shader.vertexShader = `
       attribute vec3 move;
+      attribute vec3 tint;
+
+      uniform float time;
+
+      varying vec3 vTint;
       ${shader.vertexShader}
     `;
 
     shader.vertexShader = shader.vertexShader.replace(
       '#include <begin_vertex>',
-      'vec3 transformed = vec3( position + move );',
+      `
+        vec3 transformed = vec3( position + vec3( move.xy, 150.0 * cos(time + ( move.z * ${Math.PI * 4}) ) - 75.0) );
+
+        vTint = tint / 255.0;
+      `,
+    );
+
+    shader.fragmentShader = `
+      varying vec3 vTint;
+      ${shader.fragmentShader}
+    `;
+
+    shader.fragmentShader = shader.fragmentShader.replace(
+      '#include <dithering_fragment>',
+      `
+        gl_FragColor.rgb *= vTint;
+        #include <dithering_fragment>
+      `,
     );
 
   };
-
-  // ----------------------------------------------------------------------------------
-  //
-  // create some instanced sprites
-  //
-  // ----------------------------------------------------------------------------------
-
-  const COLORS = [
-    '3ec1d3',
-    'f6f7d7',
-    'ff9a00',
-    'ff165d',
-  ];
-
-  makeCircleCoords(100, 800).forEach(([x, y]) => {
-    const sprite = spriteGroup.createSprite();
-    sprite.setMove(x, y, 0);
-    sprite.setTint(...hexCol2rgba(sample(COLORS), 180));
-  });
 
   // ----------------------------------------------------------------------------------
   //
