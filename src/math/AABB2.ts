5537f652b0d733f367d7bfff6f2c62aa1616590e
Wolfger Schramm <wolfger@spearwolf.de>
2019-03-21 13:19:15 +0100

    refactor to typescript: AABB2

--- src/math/AABB2.ts
+++ src/math/AABB2.ts
@@ -1,7 +1,16 @@
 
+/**
+ * Represents a 2D axis aligned boundary box.
+ * Uses a right-handed coordinate system.
+ */
 export class AABB2 {
 
-  constructor(left, top, width, height) {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+
+  constructor(left: number, top: number, width: number, height: number) {
     this.left = left;
     this.top = top;
     this.width = width;
@@ -18,11 +27,17 @@ export class AABB2 {
   get centerX() { return this.left + (this.width / 2); }
   get centerY() { return this.top + (this.height / 2); }
 
-  isInside(x, y) {
+  /**
+   * @returns `true` if point is within
+   */
+  isInside(x: number, y: number) {
     return this.left <= x && x < this.right && this.top <= y && y < this.bottom;
   }
 
-  isIntersecting(aabb) {
+  /**
+   * @returns `true` if the two overlap
+   */
+  isIntersecting(aabb: AABB2) {
     return !(
       aabb.right <= this.left ||
       aabb.left >= this.right ||
@@ -31,28 +46,28 @@ export class AABB2 {
     );
   }
 
-  isNorthWest(x, y) {
+  isNorthWest(x: number, y: number) {
     return (
       (this.right <= x || this.left < x) &&
       (this.top < y || this.bottom <= y)
     );
   }
 
-  isNorthEast(x, y) {
+  isNorthEast(x: number, y: number) {
     return (
       (this.right > x || this.left >= x) &&
       (this.top < y || this.bottom <= y)
     );
   }
 
-  isSouthEast(x, y) {
+  isSouthEast(x: number, y: number) {
     return (
       (this.right > x || this.left >= x) &&
       (this.top >= y || this.bottom > y)
     );
   }
 
-  isSouthWest(x, y) {
+  isSouthWest(x: number, y: number) {
     return (
       (this.right <= x || this.left < x) &&
       (this.top >= y || this.bottom > y)
