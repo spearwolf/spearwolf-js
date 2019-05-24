8d494999db4dad85eff5892b79de933cb4eadc3e
Wolfger Schramm <wolfger@spearwolf.de>
2019-05-24 09:56:54 +0200

    improve simple-sprites demo

--- examples/src/simpleSprites.js
+++ examples/src/simpleSprites.js
@@ -2,50 +2,50 @@
 /* eslint-env browser */
 import { makeExampleShell } from './utils/makeExampleShell';
 import { makeTexture } from './utils/makeTexture';
-import { debug } from './utils/debug';
 
 import {
   TextureAtlas, SimpleSprites, SimpleSpritesMaterial,
 } from '../../src';
 
 const OFFSET_Y = .1;
-const OFFSET_T = .15;
-const SCALE_X = 1.3;
-const SCALE_Y = 1;
-const MAX_X = 100;
-const MAX_Y = 100;
+const OFFSET_T = -.095;
+const OFFSET_Z = 0.3;
+const SCALE_X = 0.7;
+const SCALE_Y = .32;
+const MAX_X = 300;
+const MAX_Y = 150;
 
 const animateSprites = (sprites, tStart) => {
   for (let i = 0, t = tStart; i < sprites.length; i++, t+= OFFSET_T) {
-    const x = Math.sin(t * SCALE_X) * MAX_X;
-    const y = Math.cos((t + OFFSET_Y) * SCALE_Y) * MAX_Y;
-    sprites[i].translate(x, y, 0);
+    const tx = Math.sin(t * 0.4) % 1;
+    const x = Math.cos(((i * 0.5) + tx) * 1.3) + Math.sin(t * SCALE_X) * MAX_X;
+    const ty = Math.cos(t + (i * .3)) - (t/2);
+    const y = (Math.cos((ty + OFFSET_Y) * SCALE_Y) + Math.sin(t)) * MAX_Y;
+    sprites[i].translate(x, y, i * OFFSET_Z);
   }
 };
 
 const init = async ({ camera, scene, canvas }) => {
 
-  camera.position.y = 200;
+  camera.position.y = 400;
   camera.position.z = 0;
   camera.lookAt(0, 0, 0);
 
   const atlas = await TextureAtlas.load('amigaballs.json', '/assets/');
   const texture = makeTexture(atlas.baseTexture.imgEl);
   const material = new SimpleSpritesMaterial(texture);
-  const mesh = new SimpleSprites(material, { capacity: 100, dynamic: true, autotouch: true });
+  const mesh = new SimpleSprites(material, { capacity: 256, dynamic: true, autotouch: true });
 
   scene.add(mesh);
 
-  debug('camera', camera);
-  debug('mesh', mesh);
-
-  const COUNT = 66;
+  const COUNT = mesh.sprites.capacity;
   const FRAMES = atlas.frameNames().map(name => atlas.frame(name));
+  const FRAME_DELTA = FRAMES.length / COUNT;
 
   const sprites = [];
 
   for (let i = 0; i < COUNT; i++) {
-    sprites.push(mesh.sprites.createSpriteByTexture(FRAMES[i % FRAMES.length]));
+    sprites.push(mesh.sprites.createSpriteByTexture(FRAMES[Math.floor(i * FRAME_DELTA)]));
   }
 
   animateSprites(sprites, 0);
@@ -63,7 +63,7 @@ makeExampleShell(
   {
     alpha: true,
     autoRotate: false,
-    showCube: true,
+    showCube: false,
   },
   init,
 );
