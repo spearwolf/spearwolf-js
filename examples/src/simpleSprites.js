798454da311bf7932a4e2e9660bf2f1cd1221c7d
Wolfger Schramm <wolfger@spearwolf.de>
2019-05-28 12:36:39 +0200

    improve simple-sprites animation (snake-mode)

--- examples/src/simpleSprites.js
+++ examples/src/simpleSprites.js
@@ -3,39 +3,28 @@
 import { makeExampleShell } from './utils/makeExampleShell';
 import { makeTexture } from './utils/makeTexture';
 
-import {
-  TextureAtlas, SimpleSprites, SimpleSpritesMaterial,
-} from '../../src';
+import { TextureAtlas, SimpleSprites, SimpleSpritesMaterial } from '../../src';
 
-// const OFFSET_Y = .1;
-// const OFFSET_T = -.095;
-// const OFFSET_Z = 0.3;
-// const SCALE_X = 0.7;
-// const SCALE_Y = .32;
-// const MAX_X = 300;
-// const MAX_Y = 150;
-const OFFSET_T = 0.066;
-const OFFSET_Z = .5;
+const OFFSET_T = 0.046;
+const OFFSET_Z = .66;
+const SIZE = 400;
 
 const animateSprites = (sprites, tStart) => {
   for (let i = 0, t = tStart * .5; i < sprites.length; i++, t+= OFFSET_T) {
-    // const tx = Math.sin(t * 0.4) % 1;
-    // const x = Math.cos(((i * 0.5) + tx) * 1.3) + Math.sin(t * SCALE_X) * MAX_X;
-    // const ty = Math.cos(t + (i * .3)) - (t/2);
-    // const y = (Math.cos((ty + OFFSET_Y) * SCALE_Y) + Math.sin(t)) * MAX_Y;
-    // sprites[i].translate(x, y, i * OFFSET_Z);
-    const u = Math.cos(t); // (t % 2) - 1;
-    const v = Math.sin(t);
-    const x = Math.sin(t) * 400 * u * u;
-    const y = Math.cos(t) * 400 * v * v;
+    const a = Math.sin(Math.sin(t * .5) + ((Math.sin(t) * 1.3)) * .25) + .25;
+    const b = Math.cos(Math.cos(t * .25) + (Math.cos(t) * 2)) * .5;
+    const x = Math.sin(t) * SIZE * a;
+    const y = Math.cos(t) * SIZE * b;
+
     sprites[i].translate(x, y, i * OFFSET_Z);
+
   }
 };
 
 const init = async ({ camera, scene, canvas }) => {
 
   camera.position.y = 400;
-  camera.position.z = 50;
+  camera.position.z = 0;
   camera.lookAt(0, 0, 0);
 
   const atlas = await TextureAtlas.load('amigaballs.json', '/assets/');
@@ -47,12 +36,12 @@ const init = async ({ camera, scene, canvas }) => {
 
   const COUNT = mesh.sprites.capacity;
   const FRAMES = atlas.frameNames().map(name => atlas.frame(name));
-  const FRAME_DELTA = FRAMES.length / COUNT;
+  const FRAMES_COUNT = FRAMES.length;
 
   const sprites = [];
 
   for (let i = 0; i < COUNT; i++) {
-    sprites.push(mesh.sprites.createSpriteByTexture(FRAMES[Math.floor(i * FRAME_DELTA + (FRAME_DELTA * 2 * Math.round(Math.random() * 2))) % FRAMES.length]));
+    sprites.push(mesh.sprites.createSpriteByTexture(FRAMES[Math.floor(Math.random() * FRAMES_COUNT)]));
   }
 
   animateSprites(sprites, 0);
