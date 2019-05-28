a893fbfc6582d64472e3f1e9d0b7f01bdb24331e
Wolfger Schramm <wolfger@spearwolf.de>
2019-05-28 10:05:05 +0200

    improve simple-sprites animation

--- examples/src/simpleSprites.js
+++ examples/src/simpleSprites.js
@@ -7,20 +7,27 @@ import {
   TextureAtlas, SimpleSprites, SimpleSpritesMaterial,
 } from '../../src';
 
-const OFFSET_Y = .1;
-const OFFSET_T = -.095;
-const OFFSET_Z = 0.3;
-const SCALE_X = 0.7;
-const SCALE_Y = .32;
-const MAX_X = 300;
-const MAX_Y = 150;
+// const OFFSET_Y = .1;
+// const OFFSET_T = -.095;
+// const OFFSET_Z = 0.3;
+// const SCALE_X = 0.7;
+// const SCALE_Y = .32;
+// const MAX_X = 300;
+// const MAX_Y = 150;
+const OFFSET_T = 0.066;
+const OFFSET_Z = .5;
 
 const animateSprites = (sprites, tStart) => {
-  for (let i = 0, t = tStart; i < sprites.length; i++, t+= OFFSET_T) {
-    const tx = Math.sin(t * 0.4) % 1;
-    const x = Math.cos(((i * 0.5) + tx) * 1.3) + Math.sin(t * SCALE_X) * MAX_X;
-    const ty = Math.cos(t + (i * .3)) - (t/2);
-    const y = (Math.cos((ty + OFFSET_Y) * SCALE_Y) + Math.sin(t)) * MAX_Y;
+  for (let i = 0, t = tStart * .5; i < sprites.length; i++, t+= OFFSET_T) {
+    // const tx = Math.sin(t * 0.4) % 1;
+    // const x = Math.cos(((i * 0.5) + tx) * 1.3) + Math.sin(t * SCALE_X) * MAX_X;
+    // const ty = Math.cos(t + (i * .3)) - (t/2);
+    // const y = (Math.cos((ty + OFFSET_Y) * SCALE_Y) + Math.sin(t)) * MAX_Y;
+    // sprites[i].translate(x, y, i * OFFSET_Z);
+    const u = Math.cos(t); // (t % 2) - 1;
+    const v = Math.sin(t);
+    const x = Math.sin(t) * 400 * u * u;
+    const y = Math.cos(t) * 400 * v * v;
     sprites[i].translate(x, y, i * OFFSET_Z);
   }
 };
@@ -28,7 +35,7 @@ const animateSprites = (sprites, tStart) => {
 const init = async ({ camera, scene, canvas }) => {
 
   camera.position.y = 400;
-  camera.position.z = 0;
+  camera.position.z = 50;
   camera.lookAt(0, 0, 0);
 
   const atlas = await TextureAtlas.load('amigaballs.json', '/assets/');
@@ -45,7 +52,7 @@ const init = async ({ camera, scene, canvas }) => {
   const sprites = [];
 
   for (let i = 0; i < COUNT; i++) {
-    sprites.push(mesh.sprites.createSpriteByTexture(FRAMES[Math.floor(i * FRAME_DELTA)]));
+    sprites.push(mesh.sprites.createSpriteByTexture(FRAMES[Math.floor(i * FRAME_DELTA + (FRAME_DELTA * 2 * Math.round(Math.random() * 2))) % FRAMES.length]));
   }
 
   animateSprites(sprites, 0);
@@ -62,7 +69,7 @@ makeExampleShell(
   document.getElementById('container'),
   {
     alpha: true,
-    autoRotate: false,
+    autoRotate: true,
     showCube: false,
   },
   init,
