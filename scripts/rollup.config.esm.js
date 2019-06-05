bab3a1d6f94848921a00b1c345baad26cff741a6
Wolfger Schramm <wolfger@spearwolf.de>
2019-06-05 10:35:55 +0200

    0.0.10 use @babel/runtime helpers for transpiling

--- scripts/rollup.config.esm.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-import path from 'path';
-import config from './rollup.config.common';
-
-export default config({
-  filename: 'spearwolf',
-  root: path.resolve(__dirname, '..'),
-  outputAsModule: true,
-});
--- scripts/rollup.config.umd.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-import path from 'path';
-import config from './rollup.config.common';
-
-export default config({
-  name: 'spearwolf',
-  filename: 'spearwolf',
-  root: path.resolve(__dirname, '..'),
-});
